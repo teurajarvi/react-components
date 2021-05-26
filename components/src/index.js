6a8e53caf1ca35ff69cc0cc0d692299fcd819908
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-05-26 15:50:08 +0300

    Updated ApprovalCard and CommentDetail

--- components/src/index.js
+++ components/src/index.js
@@ -7,25 +7,30 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className='ui container comments'>
-      <ApprovalCard />
-      <CommentDetail
-        author='Sam'
-        timeAgo='Today at 12:05PM'
-        authorComment='Well done!'
-        authorImage={faker.image.avatar()}
-      />
-      <CommentDetail
-        author='Alex'
-        timeAgo='Today at 09:10AM'
-        authorComment='Great work!'
-        authorImage={faker.image.avatar()}
-      />
-      <CommentDetail
-        author='Jane'
-        timeAgo='Yesterday at 10:22AM'
-        authorComment='Keep going!'
-        authorImage={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author='Sam'
+          timeAgo='Today at 12:05PM'
+          authorComment='Well done!'
+          authorImage={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Alex'
+          timeAgo='Today at 09:10AM'
+          authorComment='Great work!'
+          authorImage={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Jane'
+          timeAgo='Yesterday at 10:22AM'
+          authorComment='Keep going!'
+          authorImage={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
